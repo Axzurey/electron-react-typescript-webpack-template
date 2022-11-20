44dd3a1ec81e8d99ce5340b115b4be676d55910f
Axzurey <66486198+Axzurey@users.noreply.github.com>
2022-11-20 13:11:45 -0500

    done.

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ export default function App() {
 
 	return (
 		<div>
-			This is a template react-typescript-electron-app!
-			Change my components and watch me reload! or
 			<Counter/>
 		</div>
 	)
